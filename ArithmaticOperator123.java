9287f48caad81a91c67b9cefaaab4920cc47ee60
Sakshi-Vaidya <110716058+Sakshi-Vaidya@users.noreply.github.com>
2023-05-06 23:56:06 +0530

    Create ArithmaticOperator123.java

---
+++ ArithmaticOperator123.java
@@ -0,0 +1,16 @@
+// program to use arithmatic operator
+
+class ArithmaticOperator123
+{
+ public static void main(String args[])
+ {
+  float a=20.5f,b=6.4f;
+  System.out.println("a="+a);
+  System.out.println("b="+b);
+  System.out.println("Addition="+(a+b));
+  System.out.println("Substraction="+(a-b));
+  System.out.println("Multiplication="+(a*b));
+  System.out.println("Division="+(a/b));
+  System.out.println("module="+(a%b));
+ }
+}
